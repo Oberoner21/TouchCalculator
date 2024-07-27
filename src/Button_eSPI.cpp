2fae2f51d05d1602bdb4647eed6915aa6867ea50
Oberoner21 <t.schindel@gmx.de>
2024-07-27 17:06:39 +0200

    neue Datei:     src/Button_eSPI.cpp 	neue Datei:     src/Button_eSPI.h 	neue Datei:     src/main.cpp

---
+++ src/Button_eSPI.cpp
@@ -0,0 +1,111 @@
+/***************************************************************************************
+** Code for the GFX button UI element
+** Grabbed from Adafruit_GFX library and enhanced to handle any label font
+***************************************************************************************/
+#include "Button_eSPI.h"
+
+Button_eSPI::Button_eSPI(void) {
+  _gfx       = nullptr;
+  _xd        = 0;
+  _yd        = 0;
+  _textdatum = MC_DATUM;
+  _label[9]  = '\0';
+  currstate = false;
+  laststate = false;
+}
+
+// Classic initButton() function: pass center & size
+void Button_eSPI::initButton(
+ TFT_eSPI *gfx, int16_t x, int16_t y, uint16_t w, uint16_t h,
+ uint16_t outline, uint16_t fill, uint16_t textcolor,
+ char *label, const GFXfont *textfont)
+{
+  // Tweak arguments and pass to the newer initButtonUL() function...
+  initButtonUL(gfx, x - (w / 2), y - (h / 2), w, h, outline, fill,
+    textcolor, label, textfont);
+}
+
+// Newer function instead accepts upper-left corner & size
+void Button_eSPI::initButtonUL(
+ TFT_eSPI *gfx, int16_t x1, int16_t y1, uint16_t w, uint16_t h,
+ uint16_t outline, uint16_t fill, uint16_t textcolor,
+ char *label, const GFXfont *textfont)
+{
+  _x1           = x1;
+  _y1           = y1;
+  _w            = w;
+  _h            = h;
+  _outlinecolor = outline;
+  _fillcolor    = fill;
+  _textcolor    = textcolor;
+  _textfont     = textfont;
+  _gfx          = gfx;
+  strncpy(_label, label, 9);
+}
+
+// Adjust text datum and x, y deltas
+void Button_eSPI::setLabelDatum(int16_t x_delta, int16_t y_delta, uint8_t datum)
+{
+  _xd        = x_delta;
+  _yd        = y_delta;
+  _textdatum = datum;
+}
+
+void Button_eSPI::drawButton(bool inverted, String long_name) {
+  uint16_t fill, outline, text;
+
+  if(!inverted) {
+    fill    = _fillcolor;
+    outline = _outlinecolor;
+    text    = _textcolor;
+  } else {
+    fill    = _textcolor;
+    outline = _outlinecolor;
+    text    = _fillcolor;
+  }
+
+  uint8_t r = min(_w, _h) / 4; // Corner radius
+  _gfx->fillRoundRect(_x1, _y1, _w, _h, r, fill);
+  _gfx->drawRoundRect(_x1, _y1, _w, _h, r, outline);
+
+  if (_gfx->textfont == 255) {
+    _gfx->setCursor(_x1 + (_w / 8),
+                    _y1 + (_h / 4));
+    _gfx->setTextColor(text);
+    //_gfx->setTextSize(_textsize);
+    _gfx->setFreeFont(_textfont);
+    _gfx->print(_label);
+  }
+  else {
+    _gfx->setTextColor(text, fill);
+    //_gfx->setTextSize(_textsize);
+    _gfx->setFreeFont(_textfont);
+
+    uint8_t tempdatum = _gfx->getTextDatum();
+    _gfx->setTextDatum(_textdatum);
+    uint16_t tempPadding = _gfx->getTextPadding();
+    _gfx->setTextPadding(0);
+
+    if (long_name == "")
+      _gfx->drawString(_label, _x1 + (_w/2) + _xd, _y1 + (_h/2) - 4 + _yd);
+    else
+      _gfx->drawString(long_name, _x1 + (_w/2) + _xd, _y1 + (_h/2) - 4 + _yd);
+
+    _gfx->setTextDatum(tempdatum);
+    _gfx->setTextPadding(tempPadding);
+  }
+}
+
+bool Button_eSPI::contains(int16_t x, int16_t y) {
+  return ((x >= _x1) && (x < (_x1 + _w)) &&
+          (y >= _y1) && (y < (_y1 + _h)));
+}
+
+void Button_eSPI::press(bool p) {
+  laststate = currstate;
+  currstate = p;
+}
+
+bool Button_eSPI::isPressed()    { return currstate; }
+bool Button_eSPI::justPressed()  { return (currstate && !laststate); }
+bool Button_eSPI::justReleased() { return (!currstate && laststate); }
---
+++ src/main.cpp
@@ -0,0 +1,262 @@
+/*
+  TouchCalculator V 1.0
+  Touchscreen Calculator auf Sunton ESP32-2432S028R
+
+  Oberoner21, Version 1.0 vom 27.07.2024
+*/
+
+// ----------------------------
+// Standard Libraries
+// ----------------------------
+#include <Arduino.h>
+#include <SPI.h>
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+#include <XPT2046_Touchscreen.h>
+// A library for interfacing with the touch screen
+//
+//https://github.com/PaulStoffregen/XPT2046_Touchscreen
+
+#include <TFT_eSPI.h>
+// A library for interfacing with LCD displays
+//
+//https://github.com/Bodmer/TFT_eSPI
+
+
+// ----------------------------
+// Touch Screen pins
+// ----------------------------
+
+// The CYD touch uses some non default SPI pins
+
+const uint8_t XPT2046_IRQ = 36;
+const uint8_t XPT2046_MOSI = 32;
+const uint8_t XPT2046_MISO = 39;
+const uint8_t XPT2046_CLK = 25;
+const uint8_t XPT2046_CS = 33;
+
+// Constants for touch calibration
+const uint16_t TS_MINX = 200;
+const uint16_t TS_MINY = 240;
+const uint16_t TS_MAXX = 3850;
+const uint16_t TS_MAXY = 3700;
+
+// ----------------------------
+
+#include "7seg.h"
+#include "orbitron.h"
+#include "Button_eSPI.h"
+
+// Objects
+SPIClass mySpi = SPIClass(VSPI);                  // SPI for touch with non default SPI pins
+XPT2046_Touchscreen ts(XPT2046_CS, XPT2046_IRQ);
+TFT_eSPI tft = TFT_eSPI();
+Button_eSPI bnCancel = Button_eSPI();
+
+// PWM constants
+const int pwmFreq = 5000;
+const int pwmResolution = 8;
+const int pwmLedChannelTFT = 0;
+const uint32_t pwmDuty = 100;     // Set brightness, max 255
+
+// Display constants
+const uint16_t screenWidth = tft.getViewportWidth();
+const uint16_t screenHeight = tft.getViewportHeight();
+char bnCancelTxT[] = {"Abbruch"};
+
+// Colors
+const uint16_t TFT_GREY = 0x5AEB; 
+const uint16_t DARKBLUE = 0x22AE;
+const uint16_t C1 = 0x02CD;
+const uint16_t C2 = 0xE280;
+const uint16_t C3 = 0x6005;
+const uint16_t BACK_COLOR = TFT_WHITE;
+
+// Calculator
+const uint8_t marginX = 12;
+const uint8_t buttonWidth = 45;
+const uint8_t buttonHeight = 45; 
+
+char buttonLabels[4][4]={{'7','8','9','/'},{'4','5','6','*'},{'1','2','3','-'},{'0','.','=','+'}};
+short buttonColors[4][4]={{C1,C1,C1,C3},{C1,C1,C1,C3},{C1,C1,C1,C3},{C1,C3,C3,C3}};
+Button_eSPI *buttons[16] = {0};
+
+uint8_t curOperation = 0;   // 0 = keine, 1 +, 2 -, 3 *, 4 /
+float n1=0;
+float n2=0;
+String num="";
+
+
+void initDraw()
+{
+  tft.fillRect(screenWidth - 53, 8, 8, 15, 0x52AA);
+  tft.fillRect(screenWidth - 43, 8, 8, 15, 0x52AA);
+  tft.fillRect(screenWidth - 33, 8, 8, 15, 0x52AA);
+  tft.fillRect(screenWidth - 23, 8, 8, 15, 0x52AA);
+  
+  tft.setTextColor(TFT_BLACK, BACK_COLOR);
+  tft.setFreeFont(&Orbitron_Bold_18);
+  tft.setCursor(marginX + 3, 22);
+  tft.print("CASIO");
+  tft.setFreeFont(&FreeMonoBold9pt7b);
+  tft.drawString("CALCULATOR", marginX + 3, 72, 2);
+  tft.fillRoundRect(marginX, 30, screenWidth - 2 * marginX, 40, 7, DARKBLUE);
+
+  // Button field
+  const uint8_t fromTop = 96;
+  uint8_t bnCount = 0;
+  uint16_t posX, posY;
+  char labelBuff[2];
+  labelBuff[1] = 0;
+  for(uint8_t i=0; i<4; i++)
+  {
+      posY = fromTop + (buttonHeight * i)+(marginX * i);
+      for(int j =0; j<4; j++)
+      { 
+          buttons[bnCount] = new Button_eSPI();
+          posX = marginX + (buttonWidth * j) + (marginX * j);
+          labelBuff[0] = buttonLabels[i][j];
+          buttons[bnCount]->initButtonUL(&tft, posX, posY, buttonWidth, buttonHeight, BACK_COLOR, buttonColors[i][j], TFT_WHITE, labelBuff, &FreeMonoBold12pt7b);
+          buttons[bnCount]->setLabelDatum(0, 2, MC_DATUM);
+          buttons[bnCount]->drawButton();
+          bnCount++;
+      }
+  }
+}
+
+void draw()
+{
+  tft.setFreeFont(&DSEG7_Classic_Bold_24);
+  tft.setTextColor(TFT_WHITE, DARKBLUE);
+  tft.fillRoundRect(marginX, 30, screenWidth - 2 * marginX, 40, 7, DARKBLUE);
+  tft.setCursor(marginX + 5, 62);
+  tft.print(num);
+}
+
+void keyPressed(char key) 
+{
+  switch(key) 
+  {
+    case '+':
+      curOperation = 1;
+      n1 = num.toFloat();
+      num = "";
+      break;
+    case '=':
+      switch(curOperation) 
+      {
+        case 1:
+          float r = n1 + num.toFloat(); 
+          num = String(r); 
+          n1 = num.toFloat();
+          int p = r * 10.00;
+          if(p % 10 == 0)
+          num = String(p / 10);
+          break;
+      }
+      break;
+    default:
+      num = num + key;
+  }
+
+  draw();
+}
+
+
+void checkTouched(TS_Point p) 
+{
+  uint8_t row, col;
+  uint16_t px = map(p.x, TS_MINX, TS_MAXX, 0, screenWidth);
+  uint16_t py = map(p.y, TS_MINY, TS_MAXY, 0, screenHeight);
+
+  for(uint8_t i=0; i<16; i++)
+  {
+    if(buttons[i]->contains(px, py))
+    {
+      if(!buttons[i]->isPressed()) 
+      {
+        buttons[i]->press(true);
+        buttons[i]->drawButton(true);
+        col = i % 4;
+        row = i / 4;
+        Serial.print("pressed: ");
+        Serial.println(buttonLabels[row][col]);
+        keyPressed(buttonLabels[row][col]);
+        break;
+      }
+    }
+    else 
+    {
+      if(buttons[i]->isPressed()) 
+      {
+        buttons[i]->press(false);
+        buttons[i]->drawButton(false);
+        Serial.println("not pressed");
+        break;
+      }
+    }
+  }
+
+  // for( auto & button : buttonArray) 
+  // {
+  //     if(px > button.xStart && 
+  //        px < button.xStart + button.width &&
+  //        py > button.yStart && 
+  //        py < button.yStart + button.height) 
+  //     {
+  //        tft.drawString(String)
+  //     }
+
+  // }
+}
+
+
+void setup() 
+{
+  Serial.begin(115200);
+  while(!Serial);
+
+  // Start the SPI for the touch screen and init the TS library
+  mySpi.begin(XPT2046_CLK, XPT2046_MISO, XPT2046_MOSI, XPT2046_CS);
+  ts.begin(mySpi);
+  ts.setRotation(0);
+
+  // Initialize the TFT display
+  tft.init();
+  tft.setRotation(0); 
+  tft.fillScreen(BACK_COLOR);
+
+  // Initialize PWM brightness
+  ledcSetup(pwmLedChannelTFT, pwmFreq, pwmResolution);
+  ledcAttachPin(TFT_BL, pwmLedChannelTFT);
+  ledcWrite(pwmLedChannelTFT, pwmDuty);
+
+  // Draw the calculator screen
+  initDraw();
+
+  // bnCancel.initButtonUL(&tft, 50, 50 , 150, 40, mainBgColor, TFT_SILVER, TFT_RED, bnCancelTxT, &FreeMonoBold12pt7b);
+  // bnCancel.setLabelDatum(0, 2, MC_DATUM);
+  // bnCancel.drawButton();
+}
+
+
+
+
+
+TS_Point nullPoint = TS_Point();
+void loop() 
+{
+
+  if (ts.tirqTouched() && ts.touched()) {
+     TS_Point p = ts.getPoint();
+     checkTouched(p);
+     delay(100);
+  }
+  else 
+  {
+    checkTouched(nullPoint);
+  }
+}
